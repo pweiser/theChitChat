196f699bf1c2261902527523b312fd9a4bf0bc62
Omar Salem <osalem@fandm.edu>
2019-03-07 18:56:07 -0500

    The original source code

---
+++ app/src/main/java/com/microphone2/osalem/thechitchat/Launch.java
@@ -0,0 +1,13 @@
+package com.microphone2.osalem.thechitchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Launch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
